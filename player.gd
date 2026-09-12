2c222966a02ee6ca9ab56f57efe1e88d7ce6bec5
Paul <paylicier@gmail.com>
2026-09-12 21:20:15 +0200

    feat: add player sfx

--- player.gd
+++ player.gd
@@ -2,13 +2,54 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
-const SNAP_SPEED = 10.0
+const SNAP_SPEED = 20.0
+const DUST_STEP_DISTANCE = 24.0
+
+enum SoundType {
+	STEP,
+	JUMP,
+	WALL_JUMP
+}
 
 @onready var visual: Node2D = $Sprite2D
 @onready var col: Node2D = $CollisionShape2D
-@export var cube_size: float = 32.0
+@onready var dust: GPUParticles2D = $DustParticles
+@onready var audio_player: AudioStreamPlayer2D = $FootstepSFX
 
+
+@export var cube_size: float = 32.0
+@export var SFX: Array[AudioStream] = []
+
+var _was_on_floor := false
+var _was_on_wall := false
+var _distance_since_dust := 0.0
 var is_magic_in_the_air: float = 0.0;
+
+var rng := RandomNumberGenerator.new()
+
+var last_sound: SoundType
+
+func play_sfx(type: SoundType) -> void:
+	if type >= SFX.size() or SFX[type] == null:
+		return
+		
+	if not audio_player.playing || last_sound != type:
+		audio_player.stream = SFX[type]
+		audio_player.play()
+		last_sound = type
+
+func _emit_dust() -> void:
+	dust.restart()
+	dust.emitting = true
+
+func _play_footstep() -> void:
+	play_sfx(SoundType.STEP)
+	
+func _play_wj() -> void:
+	play_sfx(SoundType.WALL_JUMP)
+	
+func _play_jump() -> void:
+	play_sfx(SoundType.JUMP)
 
 func _physics_process(delta: float) -> void:
 	
@@ -20,11 +61,13 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		_play_jump()
 
 	if Input.is_action_just_pressed("jump") and is_on_wall_only() and direction:
 		velocity.y = JUMP_VELOCITY
 		velocity.x = get_wall_normal().x*SPEED
 		is_magic_in_the_air = true
+		_play_wj()
 
 	if not is_magic_in_the_air:
 		if direction:
@@ -33,12 +76,31 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if is_on_floor() and not _was_on_floor:
+		_emit_dust()
+
+	if is_on_floor() and abs(velocity.x) > 1.0:
+		_distance_since_dust += abs(velocity.x) * delta
+		if _distance_since_dust >= DUST_STEP_DISTANCE:
+			_distance_since_dust = 0.0
+			_emit_dust()
+	else:
+		_distance_since_dust = 0.0
+
+	if is_on_wall() and not _was_on_wall:
+		dust.rotation = PI / 2.0 if get_wall_normal().x > 0 else -PI / 2.0
+		_emit_dust()
+
+	_was_on_floor = is_on_floor()
+	_was_on_wall = is_on_wall()
 
 	if is_on_floor():
 		is_magic_in_the_air = false
 		if abs(velocity.x) > 1.0:
 			var radius := cube_size / 2.0
 			visual.rotation += velocity.x * delta / radius
+			_play_footstep()
 		else:
 			var target : float = round(visual.rotation / (PI / 2.0)) * (PI / 2.0)
 			visual.rotation = lerp_angle(visual.rotation, target, SNAP_SPEED * delta)
