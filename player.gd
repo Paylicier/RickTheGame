804e76bb6f416a6bc7925b52707f8c4c91a41aaa
Paul <paylicier@gmail.com>
2026-09-12 15:57:24 +0200

    feat: add player rotation animation

--- player.gd
+++ player.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+const SNAP_SPEED = 10.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@onready var visual: Node2D = $Sprite2D
+@onready var col: Node2D = $CollisionShape2D
+@export var cube_size: float = 32.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	var direction := Input.get_axis("left", "right")
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		visual.rotation += direction * 6.0 * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	if is_on_floor():
+		if abs(velocity.x) > 1.0:
+			var radius := cube_size / 2.0
+			visual.rotation += velocity.x * delta / radius
+		else:
+			var target : float = round(visual.rotation / (PI / 2.0)) * (PI / 2.0)
+			visual.rotation = lerp_angle(visual.rotation, target, SNAP_SPEED * delta)
+
+	_update_pivot_offset()
+
+func _update_pivot_offset() -> void:
+	var r := cube_size / 2.0
+	var delta_angle := fposmod(visual.rotation + PI / 4.0, PI / 2.0) - PI / 4.0
+	var dist_to_bottom := r / cos(delta_angle)
+	visual.position.y = col.position.y - (dist_to_bottom - r)
